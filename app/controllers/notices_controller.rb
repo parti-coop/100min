81750a4f91e89418faafa557d89fc197b0c01f43
dalikim <dalikim@parti.xyz>
2019-02-11 17:10:43 +0900

    소식과 공지 스타일 적용합니다 https://github.com/parti-coop/100min/commit/ab931d79db45861d861601d6aca7dd0e1af97762#diff-aa3e6c9db551a905ab0fb31d69bef279

---
+++ app/controllers/notices_controller.rb
@@ -0,0 +1,10 @@
+class NoticesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'user/omniauth_callbacks' }
   root 'pages#home'
+  get :faq, to: 'pages#faq'
 
   delete :cancel_current_user, to: 'users#cancel'
   delete :confirm_user, to: 'users#confirm'
+
+  resources :stories
+  resources :notices
 end
