594fe060c92794e302d855876e76a58537c3d830
dalikim <dalikim@parti.xyz>
2019-02-21 23:35:52 +0900

    기술적인 수정 - 실서버 최초 배포 중에 마이그레이션 중에 발생한 오류를 바로 잡습니다

--- db/migrate/20190219053921_add_user_to_models.rb
+++ db/migrate/20190219053921_add_user_to_models.rb
@@ -5,9 +5,15 @@ class AddUserToModels < ActiveRecord::Migration[5.2]
     add_reference :suggestions, :user, index: true
 
     user = User.first
-    Story.update_all(user_id: user.id)
-    Notice.update_all(user_id: user.id)
-    Suggestion.update_all(user_id: user.id)
+    if user.present?
+      Story.update_all(user_id: user.id)
+      Notice.update_all(user_id: user.id)
+      Suggestion.update_all(user_id: user.id)
+    else
+      Story.destroy_all
+      Notice.destroy_all
+      Suggestion.destroy_all
+    end
 
     change_column_null :stories, :user_id, false
     change_column_null :notices, :user_id, false
