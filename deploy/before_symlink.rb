069cc284f3654bfa71a4eb3e22fece0960554d74
dalikim <dalikim@parti.xyz>
2019-02-21 23:54:11 +0900

    기술적인 수정 - letsencrypt 설정 조정

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,3 +1,4 @@
 run "rm -rf #{config.release_path}/public/uploads"
 run "ln -nfs #{config.shared_path}/public/uploads #{config.release_path}/public/uploads"
+run "ln -nfs #{config.shared_path}/letsencrypt #{config.release_path}/letsencrypt"
 
