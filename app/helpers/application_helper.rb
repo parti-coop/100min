0e057ddcd92f7b4cfc5c5cbcb66830cb1814aa8a
dalikim <dalikim@parti.xyz>
2019-03-03 12:11:29 +0900

    홈 스타일이 깨지지 않게 스타일을 바로 잡습니다

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def body_class
+    dasherized_controller_name = params[:controller].gsub(/\//, '--')
+    arr = ["app-#{dasherized_controller_name}", "app-#{dasherized_controller_name}-#{params[:action]}"]
+    arr.join(' ')
+  end
+
   def static_day_f(date)
     date.strftime("%Y.%m.%d")
   end
