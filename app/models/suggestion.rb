a5069554f3840da664232f6b0e3d9ff3224d053d
dalikim <dalikim@parti.xyz>
2019-04-30 10:51:12 +0900

    지도 콘텐츠를 추가합니다

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -22,32 +22,42 @@ class Suggestion < ApplicationRecord
     {
       code: 'KS',
       name: '수도권',
-      date: nil,
-      location: nil
+      date: '2019.7.13',
+      location: nil,
+      address: nil,
+      map_url: nil
     },
     {
       code: 'FU',
       name: '강원권',
       date: '2019.6.28',
-      location: '춘천 스카이컨벤션웨딩홀'
+      location: '춘천세종호텔 사파이어홀',
+      address: '강원도 춘천시 봉의산길31 춘천세종호텔 별관1동 2층 사파이어홀',
+      map_url: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3148.8858126442647!2d127.72905521501475!3d37.8863530135057!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3562e5e70c2b540f%3A0x3d73a6e1754b2e8!2z6rCV7JuQ64-EIOy2mOyynOyLnCDshozslpHrj5kg67SJ7J2Y7IKw6ri4IDMx!5e0!3m2!1sko!2skr!4v1556588713644!5m2!1sko!2skr'
     },
     {
       code: 'HJ',
-      name: '호남권',
+      name: '호남•제주권',
       date: '2019.6.5',
-      location: '광주 국립아시아문화전당'
+      location: '광주 국립아시아문화전당',
+      address: '광주광역시 동구 문화전당로 38 국립아시아문화전당 문화정보원 지하2층 콘퍼런스홀',
+      map_url: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3262.3991281903914!2d126.91808081493929!3d35.146665466710836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35718c84de58c767%3A0x12e1ec7dd846322a!2z6rWt66a97JWE7Iuc7JWE66y47ZmU7KCE64u5!5e0!3m2!1sko!2skr!4v1556588616672!5m2!1sko!2skr'
     },
     {
       code: 'CH',
       name: '충청권',
       date: '2019.6.10',
-      location: '대전 컨벤션센터 그랜드볼룸'
+      location: '대전 컨벤션센터 그랜드볼룸',
+      address: '대전광역시 유성구 엑스포로 107 DCC대전컨벤션센터 2층 그랜드볼룸',
+      map_url: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3212.4151543458242!2d127.38912061497251!3d36.3749426992218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x356549856f487a11%3A0xd03c3bec141f8760!2z64yA7KCE7Luo67Kk7IWY7IS87YSw!5e0!3m2!1sko!2skr!4v1556588513995!5m2!1sko!2skr'
     },
     {
       code: 'BT',
       name: '영남권',
       date: '2019.5.30',
-      location: '경남도청 대회의실(창원)'
+      location: '경남도청 대회의실(창원)',
+      address: '경상남도 창원시 의창구 중앙대로 300 경상남도청 본관 4층 대회의실',
+      map_url: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3258.7559097111016!2d128.68923011494178!3d35.23744581179012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x360df12bf0ddf1aa!2z6rK97IOB64Ko64-E7LKt!5e0!3m2!1sko!2skr!4v1556587977773!5m2!1sko!2skr'
     }
   ]
 
